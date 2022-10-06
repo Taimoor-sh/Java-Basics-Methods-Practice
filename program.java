b9a2637eb33a3309ca15602230210c2dd075c927
Taimoor-sh <106023544+Taimoor-sh@users.noreply.github.com>
2022-10-06 17:42:42 +0500

    Add files via upload
    Get values from user and display the sum ,  minus and multiplication of the values using methods for each operation

---
+++ program.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ /**
+  * program
+  */
+ public class program {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter 1st Value :"); 
+    int i = sc.nextInt();
+    System.out.println("Enter 2nd Value :");
+    int j = sc.nextInt(); 
+  add(i, j);
+  sub(i, j);
+  mul(i, j);
+  }
+ static void add(int a , int b) {
+     long c = a + b ;
+    System.out.println("Sum of both values is : " + c );
+  }
+ static void sub(int a , int b) {
+    long c = a - b ;
+   System.out.println("Subtraction of both values is : " + c );
+ }
+ static void mul(int a , int b) {
+  long c = a * b ;
+ System.out.println("Multiplication of both values is : " + c );
+}
+ }
